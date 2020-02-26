b4767a03e0edc53ddc421117664e18e5f9e7315f
Dimitris Angelakis <agelakis@gmail.com>
2020-02-26 05:18:23 +0200

    my first controller

--- app.js
+++ app.js
@@ -1,9 +1,26 @@
-(function () {
-'use strict';
+(function() {
+  'use strict';
 
-angular.module('myFirstApp', [])
-.controller('myFirstController', function(){
+  angular.module('myFirstApp', [])
+    .controller('myFirstController', function($scope) {
 
-});
+      $scope.name = 'Dima';
+      $scope.totalValue = calculateNumFromString($scope.name);
 
+      $scope.displayNumeric = function() {
+        var totalNameValue = 0;
+        // $scope.totalValue = totalNameValue;
+        $scope.totalValue = calculateNumFromString($scope.name);
+      };
+
+      function calculateNumFromString(string) {
+        var totalStringValue = 0;
+        for (var i = 0; i < string.length; i++) {
+          totalStringValue += string.charCodeAt(i);
+        }
+        return totalStringValue;
+      }
+
+
+    });
 })();
